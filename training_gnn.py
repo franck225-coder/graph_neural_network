21823e4f4c951295aadd15b87eaa6be074961c4f
franck-armand <52790721+franck225-coder@users.noreply.github.com>
2021-09-26 00:47:01 +0800

    completed training

---
+++ training_gnn.py
@@ -0,0 +1,46 @@
+###########################################################################################################
+# Training and Evaluating Simple GNNs using PyG
+###########################################################################################################
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.datasets import Planetoid
+from torch_geometric.nn import GCNConv
+
+dataset = Planetoid(root = './Cora', name = 'Cora')
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = GCNConv(dataset.num_edge_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.dropout(x, training = self.training)
+        x = self.conv2(x, edge_index)
+
+        return F.log_softmax(x, dim=1)
+
+model = Net()
+data = dataset[0]
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+model.train()
+for epoch in range(10):
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+
+model.eval()
+_, pred = model(data).max(dim=1)
+correct = int(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
+acc = correct / int(data.test_mask.sum())
+
+print('Accuracy: {:.4f}'.format(acc))
+
